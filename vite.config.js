b82a1780d56787c4c7fc878b3db71a0c368227f0
Yahia-Borham <yahya.ahmedborhan@gmail.com>
2026-04-25 09:55:35 +0300

    second release

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Food-order-app-individual-project"
+  base: process.env.VITE_BASE_PASS || "/Food-order-app-individual-project"
 })
